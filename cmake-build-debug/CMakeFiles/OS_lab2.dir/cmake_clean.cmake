988003cde0232cc2ce7e2e8d11504529388a7047
IllCher <50427226+IllCher@users.noreply.github.com>
2019-10-15 21:05:42 +0300

    update

---
+++ cmake-build-debug/CMakeFiles/OS_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_lab2.dir/main.cpp.obj"
+  "OS_lab2.exe"
+  "OS_lab2.exe.manifest"
+  "OS_lab2.pdb"
+  "libOS_lab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
